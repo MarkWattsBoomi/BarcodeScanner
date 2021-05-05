ebad9dbeda7fca643f304322da67cb58c6e278a8
Mark Watts <42679182+MarkWattsBoomi@users.noreply.github.com>
2021-05-05 16:07:55 +0100

    version 1.0

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This module contains a component which allows scanning for standard barcodes.
+This module contains a component which allows scanning for standard barcodes & QR codes.
 
 The latest version can be included in your player from this location: -
 
@@ -11,6 +11,8 @@ https://files-manywho-com.s3.amazonaws.com/e1dbcceb-070c-4ce6-95b0-ba282aaf4f48/
 ## Functionality
 
 Uses the camera on the device to scan for barcodes in various formats.
+
+If there are multiple cameras then an array of buttone is shown to select a camera.
 
 ## Component Settings
 
@@ -28,21 +30,44 @@ Like all components, adding a "classes" attribute will cause that string to be a
 Set the ScanFormats attribute to a comma separated string of barcode types to scan for.
 
 Accepted values are: -
-ean_reader
-code_128_reader
-ean_8_reader
-code_39_reader
-code_39_vin_reader
-codabar_reader
-upc_reader
-upc_e_reader
-i2of5_reader
-2of5_reader
+
+QR_CODE
+CODE_128
+CODE_39
+UPC_EAN_EXTENSION
+EAN_8
+EAN_13
+CODABAR
+CODE_93
+AZTEC
+DATA_MATRIX
+ITF
+MAXICODE
+PDF_417
+RSS_14
+RSS_EXPANDED
+UPC_A
+UPC_E
 
 Ony add the ones you need as the bigger the number the slower scanning will be.
 
-If ommitted then it defaults to just ean_reader
+## Outcomes
 
+3 outcomes will be used if defined: -
+
+### OnAccept
+This will trigger when a code is detected and the user presses the Accept button.
+Optional
+The outcome's label will be used for the accept button's label if defined.
+
+### OnCancel
+This will trigger when the user presses the cancel button.
+Optional
+The outcome's label will be used for the cancel button's label if defined
+
+### OnDetect
+This will trigger when a code is detected and bypasses the accept button mechanism.
+Optional
 
 
 
--- src/BarcodeScanner.tsx
+++ src/BarcodeScanner.tsx
@@ -1,150 +1,440 @@
-const Quagga = require('quagga');
-import { eContentType, FlowComponent, FlowObjectData, FlowObjectDataProperty } from 'flow-component-model';
+
 import * as React from 'react';
+import { BarcodeFormat, BrowserMultiFormatReader, DecodeHintType, Result } from '@zxing/library';
 
 declare const manywho: any;
 
-class BarcodeScanner extends FlowComponent {
+enum eScanStatus {
+    init,
+    scanning,
+    paused,
+    detected,
+}
 
+class BarcodeScanner extends React.Component<any,any> {
+
+    reader: BrowserMultiFormatReader;
     video: any;
+    stream: MediaStream;
     code: string;
     type: string;
-    scanning: boolean = false;
+    scanStat: eScanStatus;
     identifySuccess: boolean = false;
 
-    scanCodes: string[] = [];
+    acceptLabel: string = "Accept";
+    cancelLabel: string = "Cancel";
+    
+    outcomes: Map<string,any> = new Map();
+    attributes: Map<string,string> = new Map();
+
+    scanCodes: BarcodeFormat[] = [];
+
+    deviceIds: any = {};
+    selectedDeviceId: string = "";
+
+    cameras: any[];
 
     constructor(props: any) {
         super(props);
-        this.onBarCodeRead = this.onBarCodeRead.bind(this);
         this.startScan = this.startScan.bind(this);
         this.acceptResult = this.acceptResult.bind(this);
+        this.scanStat = eScanStatus.init;
+        this.stopScan = this.stopScan.bind(this);
+        this.cancel = this.cancel.bind(this);
+        this.switchCamera = this.switchCamera.bind(this);
+        this.buildCameras = this.buildCameras.bind(this);
+
+        let model: any = manywho.model.getComponent(this.props.id, this.props.flowKey);
+        Object.keys(model.attributes).forEach((key: string) => {
+            this.attributes.set(key,model.attributes[key]);
+        });
+        const outcomes = manywho.model.getOutcomes(this.props.id, this.props.flowKey);
+        outcomes.forEach((outcome: any) => {
+            this.outcomes.set(outcome.developerName,outcome);
+        });
+       
+
+        if(this.attributes.has("ScanFormats")) {
+            let formats: string[] = this.attributes.get("ScanFormats").split(",");
+            formats.forEach((format: string) => {
+                switch(format) {
+                    case "QR_CODE":
+                        this.scanCodes.push(BarcodeFormat.QR_CODE);
+                        break;
+                    case "CODE_128":
+                        this.scanCodes.push(BarcodeFormat.CODE_128);
+                        break;
+                    case "CODE_39":
+                        this.scanCodes.push(BarcodeFormat.CODE_39);
+                        break;
+                    case "UPC_EAN_EXTENSION":
+                        this.scanCodes.push(BarcodeFormat.UPC_EAN_EXTENSION);
+                        break;
+                    case "EAN_8":
+                        this.scanCodes.push(BarcodeFormat.EAN_8);
+                        break;
+                    case "EAN_13":
+                        this.scanCodes.push(BarcodeFormat.EAN_13);
+                        break;
+                    case "CODABAR":
+                        this.scanCodes.push(BarcodeFormat.CODABAR);
+                        break;
+                    case "CODE_93":
+                        this.scanCodes.push(BarcodeFormat.CODE_93);
+                        break;
+                    case "AZTEC":
+                        this.scanCodes.push(BarcodeFormat.AZTEC);
+                        break;
+                    case "DATA_MATRIX":
+                        this.scanCodes.push(BarcodeFormat.DATA_MATRIX);
+                        break;
+                    case "ITF":
+                        this.scanCodes.push(BarcodeFormat.ITF);
+                        break;
+                    case "MAXICODE":
+                        this.scanCodes.push(BarcodeFormat.MAXICODE);
+                        break;
+                    case "PDF_417":
+                        this.scanCodes.push(BarcodeFormat.PDF_417);
+                        break;
+                    case "RSS_14":
+                        this.scanCodes.push(BarcodeFormat.RSS_14);
+                        break;
+                    case "RSS_EXPANDED":
+                        this.scanCodes.push(BarcodeFormat.RSS_EXPANDED);
+                        break;
+                    case "UPC_A":
+                        this.scanCodes.push(BarcodeFormat.UPC_A);
+                        break;
+                    case "UPC_E":
+                        this.scanCodes.push(BarcodeFormat.UPC_E);
+                        break;
+                }
+            });
+        }
     }
 
     async componentDidMount() {
-        await super.componentDidMount();
+        
+       
         const video = this.video;
         const self = this;
-        let formats: string[] = this.getAttribute("ScanFormats","ean_reader").split(",");
-        formats.forEach((format: string) => {this.scanCodes.push(format.trim())});
-        
+  
+        const hints = new Map();
+        hints.set(DecodeHintType.POSSIBLE_FORMATS, this.scanCodes);
+        this.reader = new BrowserMultiFormatReader(hints);
 
-        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-            navigator.mediaDevices.getUserMedia({video :  {facingMode: 'environment'}})
-            .then(function(stream: any) {
-                video.srcObject = stream;
-                video.play();
-                self.startScan();
-            });
-        } else {
-
+        if(this.outcomes.has("OnAccept")) {
+            this.acceptLabel = this.outcomes.get("OnAccept").label
         }
+
+        if(this.outcomes.has("OnCancel")) {
+            this.cancelLabel = this.outcomes.get("OnCancel").label
+        }
+     
+        let devices = await navigator.mediaDevices.enumerateDevices(); 
+        for (var i = 0; i !== devices.length; ++i) {
+            var deviceInfo = devices[i];
+            if (deviceInfo.kind === 'videoinput') {
+                this.deviceIds[deviceInfo.deviceId] =  deviceInfo;
+                if(this.selectedDeviceId === "") {
+                    this.selectedDeviceId=deviceInfo.deviceId;
+                }
+            }
+        }
+        
+        self.forceUpdate();
+        await self.buildCameras();
+        self.startScan();
+        
     }
 
     async acceptResult() {
-        const acceptOutcome: string = this.getAttribute('acceptOutcome', 'accept');
-        if (this.outcomes["OnAccept"]) {
-            await this.triggerOutcome("OnAccept");
+        
+        if (this.outcomes.has("OnAccept")) {
+            manywho.engine.move(this.outcomes.get("OnAccept"), this.props.flowKey);
         }
     }
 
-    startScan() {
-        this.code = '';
-        this.type = '';
-        this.forceUpdate();
-        const video = this.video;
-        const self = this;
-        this.identifySuccess = false;
-        Quagga.init(
-            {
-                halfSample: false,
-                numOfWorkers: 0,
-                debug: {
-                    showCanvas: true,
-                    drawBoundingBox: true,
-                    showFrequency: true,
-                    drawScanline: true,
-                    showPattern: true,
-                },
-                inputStream: {
-                    name: 'live',
-                    type: 'LiveStream',
-                    target: video,
-                },
-                decoder: {
-                    readers: this.scanCodes,
-                },
-            },
-            function(err: any) {
-                if (err) {
-                    console.log(err);
-                    return;
-                } else {
-                    self.scanning = true;
-                    console.log('reading');
-                    self.forceUpdate();
-                    Quagga.onDetected(self.onBarCodeRead);
-                    Quagga.start();
-                }
-            },
-            );
+    async startScan() {
+        if (this.scanStat === eScanStatus.scanning) {
+            console.log('already scanning - can\'t start');
+            this.forceUpdate();
+        }
+        else {
+  
+            //const hints = new Map();
+            //const formats = [BarcodeFormat.QR_CODE, BarcodeFormat.DATA_MATRIX, BarcodeFormat.EAN_8, BarcodeFormat.EAN_13];
+
+            //hints.set(DecodeHintType.POSSIBLE_FORMATS, formats);
+
+            //const reader = new MultiFormatReader();
+
+            //this.reader.hints = hints;
+
+            this.scanStat = eScanStatus.scanning;
+            this.forceUpdate();
+            let result: Result = await this.reader.decodeOnceFromVideoDevice(this.selectedDeviceId, this.video) //Once(this.video); //FromStream(this.stream, this.video);//  decodeOnce(this.video);
+            this.reader.stopAsyncDecode();
+
+            this.code = result.getText();
+
+            //let state=manywho.state.getComponent(this.props.id, this.props.flowKey);
+            let newState = { "contentValue": this.code };
+            manywho.state.setComponent(this.props.id, newState, this.props.flowKey, true);
+            //this.props.onChange(result);
+            //this.props.onEvent();
+
+            this.scanStat = eScanStatus.detected;
+            this.forceUpdate();
+
+            // if outcome detected trigger it
+            if (this.outcomes.has("OnDetect")) {
+                manywho.engine.move(this.outcomes.get("OnDetect"), this.props.flowKey);
+            }
+            
+        }
     }
 
-    onBarCodeRead = (data: any) => {
-        this.code = data.codeResult.code;
-        this.setStateValue(this.code);
-        this.identifySuccess = true;
-        Quagga.stop();
-        this.scanning = false;
-        this.forceUpdate();
+    async stopScan() {
+        if (this.scanStat !== eScanStatus.scanning) {
+            console.log('not scanning - can\'t stop');
+            this.forceUpdate();
+        } else {
+            this.reader.stopAsyncDecode();
+            this.scanStat = eScanStatus.paused;
+            this.forceUpdate();
+        }
     }
 
+    async cancel() {
+        await this.stopScan();
+        if (this.outcomes.has("OnCancel")) {
+            manywho.engine.move(this.outcomes.get("OnCancel"), this.props.flowKey);
+        }
+    }
 
-    render() {
-        const text: string = this.getAttribute('title', '&copy; Boomi Flow - 2019');
-        let control: any;
-        let result: any;
+    async switchCamera(deviceId: string) {
+        await this.stopScan();
+        this.selectedDeviceId = deviceId;
+        await this.buildCameras();
+        this.startScan();
+    }
 
-        if (this.scanning === true) {
-            control = (
-                <div className="barcode-scanner-control-box">
-                    <span className="barcode-scanner-message">scanning</span>
-                    </div>
+    async buildCameras() : Promise<any> {
+
+        this.cameras=[];
+        
+        for(let deviceId of Object.keys(this.deviceIds)) {
+            let style: React.CSSProperties = {
+                width: "2rem",
+                height: "2rem",
+                backgroundColor: "transparent",
+                border: "none",
+                marginRight: "1rem",
+                fontSize: "2rem"
+
+            };
+            if(this.selectedDeviceId === deviceId) {
+                style.color="red"
+            }
+            
+            this.cameras.push(
+                
+                <div
+                    key={deviceId}
+                    onClick={(e: any) => {this.switchCamera(deviceId)}}
+                >
+                    <span 
+                        className="glyphicon glyphicon-camera" 
+                        style={style}
+                    /> 
+                </div>
             );
         }
         
-        let actionButton: any;
-        if (this.identifySuccess === true) {
-            actionButton = (
-                <button className="barcode-scanner-button" onClick={this.acceptResult}>Accept</button>
-            );
-        } 
+        return true;
+    }
 
-        if (this.code  && this.code.length > 0) {
-            result = (
-            <div className="barcode-scanner-result-box">
-                <div className="barcode-scanner-result-box-client">
-                    <div className="barcode-scanner-result-row">
-                        <span className="barcode-scanner-result">{this.code}</span>
-                    </div>
-                    <div className="barcode-scanner-result-row">
-                        <span className="barcode-scanner-button-bar">
-                            <button className="barcode-scanner-button" onClick={this.startScan}>Scan</button>
-                            {actionButton}
-                        </span>
-                    </div>
-                </div>
-            </div>
-            );
+    render() {
+        const text: string = this.attributes.has("Title")? this.attributes.get("Title") : "&copy; Boomi Flow - 2019";
+        let control: any;
+        let result: any;
+        let message: string = "";
+
+        const buttons: any = [];
+        switch (this.scanStat) {
+            case eScanStatus.init:
+                message = 'Initialising';
+                break;
+
+            case eScanStatus.scanning:
+                
+                let cancelAction: any;
+                if (this.outcomes.has("OnCancel")) {
+                    cancelAction = this.cancel;
+                }
+                else {
+                    cancelAction = this.stopScan;
+                }
+                
+                message = 'Scanning';
+                buttons.push(
+                    <button
+                        className="barcode-scanner-button"
+                        onClick={cancelAction}
+                    >
+                        {this.cancelLabel}
+                    </button>,
+                );
+                break;
+
+            case eScanStatus.paused:
+                message = 'Paused';
+                buttons.push(
+                    <button
+                        className="barcode-scanner-button"
+                        onClick={this.startScan}
+                    >
+                        Re-Scan
+                    </button>,
+                );
+                break;
+
+            case eScanStatus.detected:
+                    message = 'Code Detected';
+                    buttons.push(
+                        <button
+                            className="barcode-scanner-button"
+                            onClick={this.startScan}
+                        >
+                            Re-Scan
+                        </button>,
+                    );
+                    buttons.push(
+                        <button
+                            className="barcode-scanner-button"
+                            onClick={this.acceptResult}
+                        >
+                            {this.acceptLabel}
+                        </button>,
+                    );
+                    result = this.code;
+                    break;
         }
+        
+        let offset: string = "4rem"; 
+        if(result?.length > 0) {
+            offset="7rem";
+        }
+        control = (
+            <div
+                style={{
+                    position: 'absolute',
+                    left: this.video?.clientLeft,
+                    top: this.video?.clientTop,
+                    height: this.video?.clientHeight,
+                    width: this.video?.clientWidth,
+                    zIndex: 1000,
+                    display: 'flex',
+                    flexDirection: 'column',
+                }}
+            >
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    flexGrow: 0,
+                    backgroundColor: '#000000',
+                    height: "3rem"
+                }}
+            >
+                <span
+                    style={{
+                        margin: 'auto',
+                        fontFamily: 'tahoma',
+                        color: '#fefefe',
+                        fontSize: '1.4rem'
+                    }}
+                >
+                    {message}
+                </span>
+            </div>
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    flexGrow: 0,
+                    backgroundColor: '#000000'
+                }}
+            >
+                <div
+                    style={{
+                        margin: 'auto',
+                    }}
+                >
+                    <span
+                        style={{
+                            margin: 'auto',
+                            fontFamily: 'tahoma',
+                            color: '#fefefe',
+                            fontSize: '2rem'
+                        }}
+                    >
+                        {result}
+                    </span>
+                </div>
+
+            </div>
+                <div
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        flexGrow: 1,
+                    }}
+                >
+                    <div
+                        style={{
+                            margin: 'auto',
+                        }}
+                    >
+                        {buttons}
+                    </div>
+
+                </div>
+                <div
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        flexGrow: 1,
+                        position: "absolute",
+                        top: offset,
+                        right: 0
+                    }}
+                >
+                    {this.cameras}
+                </div>
+
+            </div>
+        );
+
         return (
             <div className="barcode-scanner">
-                <video ref={(me: any) => {this.video = me; }} className="barcode-scanner-canvas" autoPlay={true}>
-                </video>
+                <video 
+                    ref={(me: any) => {this.video = me; }} 
+                    className="barcode-scanner-canvas" 
+                    autoPlay={true}
+                    style={{
+                        position: 'absolute',
+                        left: 0,
+                        top: 0,
+                        width: '100%',
+                    }}
+                    
+                />
                 {control}
-                {result}
             </div>
-                );
+        );
     }
 
 }
